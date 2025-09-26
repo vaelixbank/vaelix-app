d4bf45b9be0c0b2f6b68c6fdf3d72353990b7b62
Liam Von Astoria <hello@liamvonastoria.net>
2025-09-26 16:12:54 +0200

    Add Capacitorjs Framework for mobile version

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vaelix.vbgapp',
+  appName: 'vaelixbank',
+  webDir: 'public'
+};
+
+export default config;
